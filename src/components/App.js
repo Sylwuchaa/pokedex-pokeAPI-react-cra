03143297a2d83831c8b4cd2b3d29d667aa1ef2bd
Sylwcuhaa <uzumagi1@gmail.com>
2019-02-08 14:18:46 +0100

    styled components basic

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 import Result from "./Result";
 import Form from "./Form";
 import Title from "./Title";
 import Photo from "./Photo";
 import "./App.css";
 
+const StyledWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  background-image: linear-gradient(
+    to right top,
+    #051937,
+    #004d7a,
+    #008793,
+    #00bf72,
+    #a8eb12
+  );
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  width: 100%;
+  text-align: center;
+  margin: 0 auto;
+  color: white;
+  text-shadow: 2px 2px black;
+`;
 class App extends Component {
   state = {
     value: "",
@@ -16,7 +39,7 @@ class App extends Component {
     base_experience: "",
     abilities: "",
     images: [],
-    err: false,
+    err: false
   };
 
   handleChange = e => {
@@ -49,21 +72,21 @@ class App extends Component {
           base_experience: data.base_experience,
           abilities: data.abilities[0].ability.name,
           images: [
-          {name: 'front default', id: 0, src: data.sprites.front_default},
-          {name:'back default', id: 1, src: data.sprites.back_default},
-          {name: 'front female', id: 2, src: data.sprites.front_female},
-          {name: 'back female', id: 3, src: data.sprites.back_female}
+            { name: "front default", id: 0, src: data.sprites.front_default },
+            { name: "back default", id: 1, src: data.sprites.back_default },
+            { name: "front female", id: 2, src: data.sprites.front_female },
+            { name: "back female", id: 3, src: data.sprites.back_female }
           ],
-          err: false,
-        }))
+          err: false
+        }));
       })
       .catch(err => {
-        console.log(err)
+        console.log(err);
         this.setState(prevState => ({
-          err: true,
-        }))
-      })
-    }
+          err: true
+        }));
+      });
+  };
   handlePokemonSubmit = e => {
     console.log("wysłane");
     e.preventDefault();
@@ -87,25 +110,25 @@ class App extends Component {
           base_experience: data.base_experience,
           abilities: data.abilities[0].ability.name,
           images: [
-          {name: 'front default', id: 0, src: data.sprites.front_default},
-          {name:'back default', id: 1, src: data.sprites.back_default},
-          {name: 'front female', id: 2, src: data.sprites.front_female},
-          {name: 'back female', id: 3, src: data.sprites.back_female}
+            { name: "front default", id: 0, src: data.sprites.front_default },
+            { name: "back default", id: 1, src: data.sprites.back_default },
+            { name: "front female", id: 2, src: data.sprites.front_female },
+            { name: "back female", id: 3, src: data.sprites.back_female }
           ],
-          err: false,
-        }))
+          err: false
+        }));
       })
       .catch(err => {
-        console.log(err)
+        console.log(err);
         this.setState(prevState => ({
-          err: true,
-        }))
-      })
-  }
+          err: true
+        }));
+      });
+  };
 
   render() {
     return (
-      <>
+      <StyledWrapper>
         <Title />
         <Form
           value={this.state.value}
@@ -113,9 +136,9 @@ class App extends Component {
           submit={this.handlePokemonSubmit}
           click={this.handleClickRnd}
         />
-        <Result pokemon={this.state}/>
+        <Result pokemon={this.state} />
         <Photo photo={this.state} />
-      </>
+      </StyledWrapper>
     );
   }
 }
