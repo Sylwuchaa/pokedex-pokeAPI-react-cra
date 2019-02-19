342cb8d0679a3504f0efa46ae102f3d29e5f273d
Sylwcuhaa <uzumagi1@gmail.com>
2019-02-19 21:02:56 +0100

    added info content

--- src/components/Info.js
+++ src/components/Info.js
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 const InfoBox = styled.div`
   margin: .2rem;
   background: rgba(0,0,0,.4);
-  width: 470px;
+  width: 490px;
 
   border-radius: 1rem;
   box-shadow: 0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22);
@@ -19,7 +19,10 @@ const InfoBox = styled.div`
 
 const Info = () => (
   <>
-  <InfoBox>Info</InfoBox>
+  <InfoBox>
+  <h3>Info</h3>
+  <p>This a small Pokémon Encyclopedia written in React.js and Create-React-App, using external data APIs from <a href="https://pokeapi.co/">https://pokeapi.co/</a></p>
+  </InfoBox>
   </>
 )
 
