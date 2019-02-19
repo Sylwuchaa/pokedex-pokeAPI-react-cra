39a34a73ae913f29c5f693a4b602ef349882c836
Sylwcuhaa <uzumagi1@gmail.com>
2019-02-19 21:11:22 +0100

    fix info style

--- src/components/Info.js
+++ src/components/Info.js
@@ -5,8 +5,9 @@ import styled from 'styled-components';
 const InfoBox = styled.div`
   margin: .2rem;
   background: rgba(0,0,0,.4);
-  width: 490px;
-
+  padding: 1.8rem;
+  width: 30%;
+  height: 15%;
   border-radius: 1rem;
   box-shadow: 0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22);
 
@@ -21,7 +22,7 @@ const Info = () => (
   <>
   <InfoBox>
   <h3>Info</h3>
-  <p>This a small Pokémon Encyclopedia written in React.js and Create-React-App, using external data APIs from <a href="https://pokeapi.co/">https://pokeapi.co/</a></p>
+  <p>This a small Pokémon Encyclopedia written in React.js and Create-React-App, using external data APIs from: <a href="https://pokeapi.co/">https://pokeapi.co/</a></p>
   </InfoBox>
   </>
 )
